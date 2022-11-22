96e1e2546f29b599601b528e4e1262e1f750b70b
Konrad Kapica <konradkapica@gmail.com>
2022-11-22 22:08:34 +0100

    currencies added to array

---
+++ src/Currencies/index.js
@@ -0,0 +1,22 @@
+const currecies = [
+    {
+        name: "Euro",
+        short: "EUR",
+        rate: 4.77,
+    },
+    {
+        name: "Dolar amerykański",
+        short: "USD",
+        rate: 4.84,
+    },
+    {
+        name: "Funt brytyjski",
+        short: "GBP",
+        rate: 5.46,
+    },
+    {
+        name: "Hrywna",
+        short: "UAH",
+        rate: 0.13,
+    },
+];
