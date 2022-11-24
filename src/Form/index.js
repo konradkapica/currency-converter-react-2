c8c1ca32c8a1312b54ba2683cdb343247a7c3ecd
Konrad Kapica <konradkapica@gmail.com>
2022-11-24 01:10:41 +0100

    input controlled

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,41 +1,61 @@
 import "./style.css";
 import { currecies } from "../Currencies";
 import Header from "../Header";
+import { useState } from "react";
 
-const Form = (props) => (
-    <form className="form">
-        <fieldset className="form__fieldset">
-            <Header
-             title="Przelicznik walut"
-            />
-            <p className="form__paragraph">
-                <label>
-                    <span className="form__labelText">Wpisz kwotę (PLN):</span>
-                    <input className="form__field" name="PLN" type="number" min="0.01" step="any"
-                        placeholder="PLN" autoFocus required />
-                </label>
-            </p>
-            <p className="form__paragraph">
-                <label>
-                    <span className="form__labelText">Wybierz walutę:</span>
-                    <select className="form__field" name="currency">
-                        {currecies.map(currency => (
-                            <option
-                                key={currency.short}
-                                value={currency.short}
-                            >
-                                {currency.name}
-                            </option>
-                        ))};
-                    </select>
-                </label>
-            </p>
-            <p className="form__paragraph">
-                <button className="form__button">Przelicz</button>
-            </p>
-        </fieldset>
+const Form = (props) => {
+    const [amount, setAmount] = useState("")
 
-    </form>
-);
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <fieldset className="form__fieldset">
+                <Header
+                    title="Przelicznik walut"
+                />
+                <p className="form__paragraph">
+                    <label>
+                        <span className="form__labelText">Wpisz kwotę (PLN):</span>
+                        <input
+                            className="form__field"
+                            name="PLN"
+                            type="number"
+                            min="0.01"
+                            step="any"
+                            placeholder="PLN"
+                            autoFocus
+                            required
+                            value={amount}
+                            onChange={({target}) => setAmount(target.value)}
+                        />
+                    </label>
+                </p>
+                <p className="form__paragraph">
+                    <label>
+                        <span className="form__labelText">Wybierz walutę:</span>
+                        <select className="form__field" name="currency">
+                            {currecies.map(currency => (
+                                <option
+                                    key={currency.short}
+                                    value={currency.short}
+                                >
+                                    {currency.name}
+                                </option>
+                            ))};
+                        </select>
+                    </label>
+                </p>
+                <p className="form__paragraph">
+                    <button className="form__button">Przelicz</button>
+                </p>
+            </fieldset>
+
+        </form>
+    );
+};
+
 
 export default Form;
