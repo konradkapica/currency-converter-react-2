67b5ac0ca2d7b54e0da386062d2a17f56ed6ab55
Konrad Kapica <konradkapica@gmail.com>
2022-11-24 18:56:55 +0100

    destructurisation in calculateResult function

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import Form from "./Form";
 import Result from "./Result";
 import Header from "./Header";
 import { useState } from "react";
-import { currencies } from "./Currencies";
+import { currencies } from "./currencies";
 
 
 function App() {
   const [result, setResult] = useState(null);
 
   const calculateResult = (amount, currency) => {
-    const rateExchange = currencies.find(({ short }) => short === currency).rate;
-    const currencyName = currencies.find(({ short }) => short === currency).short;
+    const { rate: rateExchange, short: currencyName } = currencies.find(({ short }) => short === currency);
     setResult((`${amount} PLN = `) + (+amount / rateExchange).toFixed(2) + ` ${currencyName}`);
   };
 
   return (
     <Container>
-      <Header title="Przelicznik walut"/>
+      <Header title="Przelicznik walut" />
       <Form
         calculateResult={calculateResult}
       />
