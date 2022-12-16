4b0172f24b37254762677fc4f8050e8efe4369e0
Konrad Kapica <konradkapica@gmail.com>
2022-12-16 11:15:02 +0100

    styled components added to Result

--- src/Result/styled.js
+++ src/Result/styled.js
@@ -1,6 +1,8 @@
-.result {
+import styled from "styled-components";
+
+export const StyledResult = styled.p`
     text-align: center;
     margin: 0;
     color: hsl(180, 100%, 25%);
     font-size: 17px;
-}
+`;
