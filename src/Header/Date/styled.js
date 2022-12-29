8f2c9c2921db742a8ca042df37625c2ff69ffcf6
Konrad Kapica <konradkapica@gmail.com>
2022-12-29 15:05:01 +0100

    Date style changed to monospace

--- src/Header/Date/styled.js
+++ src/Header/Date/styled.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const StyledClock = styled.p`
-    font-size: 14px;
+    font-family: monospace;
+    font-size: 12px;
     text-align: right;
     margin: 0;
+    margin-bottom: 20px;
 `;
