1686ca06d51e63c3c7fe3b1f3a191cdf76e8ffb4
Konrad Kapica <konradkapica@gmail.com>
2022-12-29 15:04:29 +0100

    Result moved to Form

---
+++ src/Form/Result/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledResult = styled.p`
+    text-align: center;
+    margin: 0;
+    margin-top: 15px;
+    color: hsl(180, 100%, 25%);
+    font-size: 17px;
+`;
