a7eded5c8eb167d4ed93eed4f5351122946ded03
Konrad Kapica <konradkapica@gmail.com>
2022-12-29 15:03:20 +0100

    useRatesData using API and added to Form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,15 @@
-import { currencies } from "../currencies";
 import { useState } from "react";
-import { StyledForm, Paragraph, LabelText, FormFiled, Button } from "./styled"
+import { StyledForm, Paragraph, LabelText, FormFiled, Button, ErrorText, LoadingText, Spinner, Info } from "./styled"
+import { Result } from "./Result"
+import { useRatesData } from "./useRatesData";
 
-const Form = ({ calculateResult }) => {
+
+const Form = () => {
     const [amount, setAmount] = useState("");
-    const [currencyExchange, setCurrencyExchange] = useState(currencies[0].short);
+    const [currencyExchange, setCurrencyExchange] = useState("EUR");
+    const [result, setResult] = useState("");
+    const ratesData = useRatesData();
+    const { date, rates, status } = ratesData;
 
     const onSelectChange = ({ target }) => setCurrencyExchange(target.value);
 
@@ -13,9 +18,35 @@ const Form = ({ calculateResult }) => {
         calculateResult(amount, currencyExchange);
     };
 
+    const calculateResult = (amount, currency) => {
+        const rateExchange = rates[currency];
+
+        setResult({
+            sourceAmount: +amount,
+            targetAmount: +amount * rateExchange,
+            currency,
+        });
+    };
+
     return (
-            <StyledForm onSubmit={onFormSubmit}>
-                <Paragraph>
+        <>
+        <StyledForm onSubmit={onFormSubmit}>
+            {status === "error" ? (
+                <ErrorText>
+                    Ups... coś poszło nie tak!
+                    <br />
+                    Możesz nie mieć połączenia z internetem. Odśwież stronę lub spróbuj później.
+                </ErrorText>
+            ) : status !== "success" ? (
+                <>
+                    <LoadingText>
+                        Trwa ładowanie danych z Europejskiego Banku Centralnego...
+                    </LoadingText>
+                    <Spinner></Spinner>
+                </>
+            ) : (
+                <>
+                    <Paragraph>
                         <LabelText>Wpisz kwotę (PLN):</LabelText>
                         <FormFiled
                             name="PLN"
@@ -28,8 +59,8 @@ const Form = ({ calculateResult }) => {
                             value={amount}
                             onChange={({ target }) => setAmount(target.value)}
                         />
-                </Paragraph>
-                <Paragraph>
+                    </Paragraph>
+                    <Paragraph>
                         <LabelText>Wybierz walutę:</LabelText>
                         <FormFiled
                             as="select"
@@ -37,20 +68,29 @@ const Form = ({ calculateResult }) => {
                             value={currencyExchange}
                             onChange={onSelectChange}
                         >
-                            {currencies.map(currency => (
+                            {Object.keys(rates).map(currency => (
                                 <option
-                                    key={currency.short}
-                                    value={currency.short}
+                                    key={currency}
+                                    value={currency}
                                 >
-                                    {currency.name}
+                                    {currency}
                                 </option>
                             ))};
                         </FormFiled>
-                </Paragraph>
-                <Paragraph>
+                    </Paragraph>
                     <Button>Przelicz</Button>
-                </Paragraph>
-            </StyledForm>
+                </>
+            )}
+        </StyledForm>
+        <Result
+        result={result}
+    />
+    <Info>
+        Kursy walut pobierane są z Europejskiego Banku Centralnego.
+        <br />
+        Aktualne na dzień: <strong>{date}</strong>
+    </Info>
+    </>
     );
 };
 
---
+++ src/Form/useRatesData.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+export const useRatesData = () => {
+    const [ratesData, setRatesData] = useState({ status: "loading" });
+
+    useEffect(() => {
+        const getRates = async () => {
+            try {
+                const currenciesURL = "https://api.exchangerate.host/latest?base=PLN";
+                const response = await axios.get(currenciesURL)
+
+                setRatesData({
+                    date: response.data.date,
+                    rates: response.data.rates,
+                    status: "success"
+                });
+            } catch (error) {
+                console.error(error);
+                setRatesData({
+                    status: "error",
+                });
+            }
+        };
+
+        setTimeout(getRates, 2000);
+    }, []);
+
+    return ratesData;
+};
