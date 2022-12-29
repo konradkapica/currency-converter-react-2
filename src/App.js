6ed18f2a1b269590359c3b18cdbb829c3a0412ff
Konrad Kapica <konradkapica@gmail.com>
2022-12-29 15:01:33 +0100

    Result moved to Form

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
 import Container from "./Container";
 import Form from "./Form";
-import { Result } from "./Result"
 import Header from "./Header";
-import { useState } from "react";
-import { currencies } from "./currencies";
 
 
 function App() {
-  const [result, setResult] = useState("");
-
-  const calculateResult = (amount, currency) => {
-    const rateExchange = currencies.find(({ short }) => short === currency).rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: +amount / rateExchange,
-      currency,
-    });
-  };
 
   return (
     <Container>
       <Header title="Przelicznik walut" />
-      <Form
-        calculateResult={calculateResult}
-      />
-      <Result
-        result={result}
-      />
+      <Form/>
     </Container>
   );
-}
+};
 
 export default App;
