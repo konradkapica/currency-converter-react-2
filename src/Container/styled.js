fee851ce4c9a5bdfda036d79adf44d2da664867f
Konrad Kapica <konradkapica@gmail.com>
2023-01-04 12:11:03 +0100

    min-height of Container added

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const StyledContainer = styled.main`
     max-width: 500px;
+    min-height: 440px;
     border: 1px solid #ccc;
     border-radius: 10px;
     padding: 20px;
@@ -9,7 +10,7 @@ export const StyledContainer = styled.main`
     background-color: rgba(255, 255, 255, 0.887);
     box-shadow: 0px 0px 86px -33px #606060;
     display: grid;
-    grid-gap: 10px;
+    grid-gap: 20px;
 
     @media (max-width: 531px) {
         margin: 50px 15px auto 15px;
